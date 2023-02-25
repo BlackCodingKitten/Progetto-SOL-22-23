6772de26458be279549e02463ffd5f3e47cb4e78
Michela <m.deodati@studenti.unipi.it>
2023-02-25 16:13:28 +0100

    la funzione di lettura del file finalmente funziona

--- FunzioniDiProva/LeggiFileeCalcola.c
+++ FunzioniDiProva/LeggiFileeCalcola.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+#define BUFFERSIZE 1024
 
 
 
@@ -12,7 +12,8 @@ typedef struct data{
     string path;
 }Data;
 
-long calcolaFile(FILE *file);
+long getFileSize(FILE *file);
+long calcolaFile(FILE *file,string path);
 int compare(const void*a, const void* b);
 
 int main (int argc,string argv[]){
@@ -26,7 +27,7 @@ int main (int argc,string argv[]){
 
      //apro i file;
      for(int i=0; i< (argc-1); i++){
-        fileArray[i]=fopen(argv[i+1], "r");
+        fileArray[i]=fopen(argv[i+1], "rb");
         if(fileArray[i]==NULL){
             perror("fopen()");
             return EXIT_FAILURE;
@@ -37,59 +38,46 @@ int main (int argc,string argv[]){
     for(int i=0;i<(argc-1);i++){
         resArray[i].path=malloc(sizeof(char)*(strlen(argv[i+1])+1));
         strcat(resArray[i].path, argv[i+1]);
-        resArray[i].value=calcolaFile(fileArray[i]);
+        resArray[i].value=calcolaFile(fileArray[i],argv[i+1]);
     }
     qsort(resArray, (argc-1), sizeof(Data), compare);
     for(int x=0; x<(argc-1); x++){
-        fprintf(stdout, "%ld\t%s\n", resArray[x].value,resArray[x].path);
+         fprintf(stdout, "%ld\t%s\n", resArray[x].value,resArray[x].path);
     }
     for(int y=0; y<(argc-1);y++){
         fclose(fileArray[y]);
     }
-    if(fileArray!=NULL){
-        free(fileArray);
-    }
     return 0;
 }
 
-long calcolaFile(FILE*file){
-    long value [100];
-    for(int i=0; i<100; i++){
-        value[i]=-1;
-    }
-    string s = malloc(sizeof(char)*100);
-    string e=NULL;
-    int i=0;
-    while(fgets(s,100,file)!=NULL){
-        e=NULL;
-        string temp =malloc(sizeof(char)*(strlen(s)+1));
-        if(strstr(s,"\n")){
-            strncpy(temp,s,strlen(s)-1);
-        }
-        value[i]=strtol(temp,&e,0);
-        if(*e==(char)0 && e!=NULL){
-            value[i]=i*value[i];
-        }else{
-            fprintf(stderr, "ERRORE nel file");
-            exit(EXIT_FAILURE);
-        }
-        i++;
-        free(temp);
-        memset(s,'\0', 100);
-    }
+long calcolaFile(FILE*file,string path){
+    long somma=0;
+    string buffer = malloc(sizeof(char)*BUFFERSIZE);
+    long dim = getFileSize(file);
+    //printf("file size %ld \n",dim);
+    dim = dim / sizeof(long);
+    //printf("long in file  %ld \n",dim);
+    //alloco l'array in cui salvare i valori:
+    long* fileLong = (long*)malloc(dim*sizeof(long));
+    //leggiamo i valori nel file
+    fread(fileLong,sizeof(long),dim,file);
 
-    long somma = 0;
-    i=0;
-    while(value[i]!=-1){
-        somma=somma+value[i];
-        i++;
+    for(int i=0; i<dim; i++){
+        somma=somma+(fileLong[i]*i);
     }
-   if(s!=NULL){
-    free(s);
-   }
-   return somma;
+    return somma;
 }
 
 int compare (const void* a, const void* b){
     return((*(Data*)a).value - (*(Data*)b).value);
+}
+
+long getFileSize(FILE *file) {
+    long size;
+
+    fseek(file, 0, SEEK_END);  // si posiziona alla fine del file
+    size = ftell(file);        // legge la posizione attuale nel file, che corrisponde alla sua dimensione in byte
+    fseek(file, 0, SEEK_SET);  // si riporta all'inizio del file
+
+    return size;
 }
